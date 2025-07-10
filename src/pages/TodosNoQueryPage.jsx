dcc8b89461b9699a4c81efafbdbb4050f445b0d2
seungju <lsjxcs@gmail.com>
2025-07-10 20:00:21 +0900

    session

--- src/pages/TodosNoQueryPage.jsx
+++ src/pages/TodosNoQueryPage.jsx
@@ -1,10 +1,28 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
+import axios from 'axios';
 
 export default function TodosNoQueryPage() {
   const [todos, setTodos] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
   const [isError, setIsError] = useState(null);
+
+  useEffect(() => {
+    const fetchTodos = async () => {
+      try {
+        setIsLoading(true);
+        const res = await axios.get(
+          'https://jsonplaceholder.typicode.com/todos'
+        );
+        setTodos(res.data);
+      } catch (error) {
+        setIsError(error.message);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+    fetchTodos();
+  }, []);
 
   return (
     <div className="p-6">
