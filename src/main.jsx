8bd7d4b3b50c58d4bc1326d3eb926d5a89028b9d
seungju <lsjxcs@gmail.com>
2025-07-10 20:04:43 +0900

    fix

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,8 @@ import './index.css';
 import App from './App.jsx';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
-import {Analytics}
+import { Analytics } from '@vercel/analytics/react';
+import { SpeedInsights } from '@vercel/speed-insights/react';
 
 const queryClient = new QueryClient();
 
@@ -12,8 +13,8 @@ createRoot(document.getElementById('root')).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
       <App />
-      <Analytics/>
-      <SpeedInsight/>
+      <Analytics />
+      <SpeedInsights />
       <ReactQueryDevtools initialIsOpen={true} />
     </QueryClientProvider>
   </StrictMode>
